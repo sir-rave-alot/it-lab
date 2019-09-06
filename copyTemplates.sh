414d88f565a24a5d547569056c002d494ab3170a
Raphael <raphael.andonie@students.unibe.ch>
2019-09-06 11:38:26 +0200

    add copytemplates

---
+++ copyTemplates.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#
+# REPLACE KEYWORDS IN FILE
+# FOR RASPBERRY PI
+#
+# RAPHAEL ANDONIE, HSLU 2019
+
+################################################################################
+# SIMPLE CHECK IF VARIABLE.SH HAS BEEN RUN IN THIS SHELL
+if [ -z "${_VAR_IS_RUN}" ];
+then
+    echo 'Please define Variables.'
+    echo 'This is normally done by running variables.sh'
+    exit ${_ERR_FAIL}
+fi
+
+# CREATE OUTPUT DIRECTORY IF NOT EXISTING
+mkdir -p ${_OBJ_DIR}
+
+# COPY TEMPLATE DIRECTORY FILES
+_TEMPLATES=($(ls ${_TEMPLATE_DIR}))
+_NOF_TPL=${#_TEMPLATES[@]}
+
+for (( i=0; i<${_NOF_TPL}; i++ ));
+do
+	echo "Copy " ${_TEMPLATES[$i]}
+	cp "${_TEMPLATE_DIR}/${_TEMPLATES[$i]}" ${_OBJ_DIR}/"${_TEMPLATES[$i]#TEMPLATE_PREFIX}"
+	#echo "${_TEMPLATE_DIR}/${_TEMPLATES[$i]}"
+	#cp ${_TEMPLATE_DIR}/${_TEMPLATES[$i]} ${PRJ_PATH}/${OBJ_DIR}/${FILENAME}
+
+done
+
+exit 0
+# TEMPLATE TO WORKING COPY
+_CPY_SRC=${PRJ_PATH}/${TEMPLATE_DIR}/${TEMPLATE_PREFIX}${FILENAME}
+_CPY_W=${PRJ_PATH}/${OBJ_DIR}/${FILENAME}
+
+cp ${_CPY_SRC} ${_CPY_W}
+
+echo "Template ${_CPY_SRC} copied to ${_CPY_W}"
+exit ${_ERR_OK}
+
+
--- variables.sh
+++ variables.sh
@@ -46,6 +46,7 @@ export _IMG_PATH=${PRJ_PATH}/${IMG_DIR}/${IMG_FILE}
 export _KEYS_SUFFIX=".keys"
 
 export _OBJ_DIR=${PRJ_PATH}/${OBJ_DIR}
+export _TEMPLATE_DIR=${PRJ_PATH}/${TEMPLATE_DIR}
 
 export _BOOT_MNT_DIR=${PRJ_PATH}/${BOOT_MNT_DIR}
 export _RFS_MNT_DIR=${PRJ_PATH}/${RFS_MNT_DIR}
